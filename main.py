bd2b569eec7fbca94411dccc552b025745217954
naranjujeli <lucadive@gmail.com>
2023-09-18 20:48:32 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+from tcp import Nodo, Estado, Flag
+OK   = '\033[92m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+
+# Modificaciones nunca se pasa por el estado TIME_WAIT (se va directo a CLOSED) y no se pasa por el estado CLOSE_WAIT (se va al estado LAST_ACK)
+
+def test1():
+	# Test: El estado LISTEN funciona bien ante el flag SYN
+	cliente = Nodo()
+	servidor = Nodo(Estado.LISTEN)
+	cliente.send(servidor, [Flag.SYN])
+
+	# Se fija que al enviar el flag SYN a un nodo en estado LSITEN, este cambie a estado SYN_RCDV
+	assert(servidor.estado == Estado.SYN_RCDV), f'El servidor no cambio a estado SYN_RCDV'
+
+def test2():
+	# Test: El estado SYN_SENT pasa a ESTABLISHED si se le envia un SYN + ACK
+	cliente = Nodo(Estado.SYN_SENT)
+	servidor = Nodo(Estado.LISTEN)
+	servidor.send(cliente, [Flag.SYN, Flag.ACK])
+
+	# Se fija que al enviar los flags SYN + ACK el cliente queda en estado ESTABLISHED
+	assert(cliente.estado == Estado.ESTABLISHED), f'El cliente no cambio a estado ESTABLISHED'
+
+def test3():
+	# Test: El estado SYN_RCDV pasa a ESTABLISHED si se le envia un ACK
+	cliente = Nodo()
+	servidor = Nodo(Estado.LISTEN)
+	cliente.send(servidor, [Flag.SYN])
+	cliente.send(servidor, [Flag.ACK])
+
+	# Se fija que al enviar el flag ACK el servidor queda en estado ESTABLISHED
+	assert(servidor.estado == Estado.ESTABLISHED), f'El servidor no cambio a estado ESTABLISHED'
+
+def test4():
+	# Test: El estado LAST_ACK pasa a CLOSED si se le envia un ACK
+	cliente = Nodo(Estado.ESTABLISHED)
+	servidor = Nodo(Estado.ESTABLISHED)
+	servidor.send(cliente, [Flag.FIN])
+	servidor.send(cliente, [Flag.ACK])
+
+	# Se fija que al enviar el flag ACK el cliente queda en estado CLOSED
+	assert(cliente.estado == Estado.CLOSED), f'El cliente no cambio a estado CLOSED'
+
+def test5():
+	cliente = Nodo(Estado.ESTABLISHED)
+	servidor = Nodo(Estado.FIN_WAIT_1)
+	servidor.send(cliente, [Flag.FIN])
+	
+	# Se fija que al enviar el flag FIN el cliente queda en estado CLOSING
+	assert(cliente.estado == Estado.CLOSING), f'El cliente no cambio a estado CLOSING'
+
+def test6():
+	cliente = Nodo(Estado.ESTABLISHED)
+	servidor = Nodo(Estado.FIN_WAIT_1)
+	servidor.send(cliente, [Flag.FIN])
+	servidor.send(cliente, [Flag.ACK])
+	
+	# Se fija que al enviar el flag ACK el cliente queda en estado CLOSED
+	assert(cliente.estado == Estado.CLOSED), f'El cliente no cambio a estado CLOSED'
+
+def test7():
+	cliente = Nodo(Estado.ESTABLISHED)
+	servidor = Nodo(Estado.FIN_WAIT_1)
+	cliente.send(servidor, [Flag.ACK])
+	
+	# Se fija que al enviar el flag ACK el servidor queda en estado FIN_WAIT_2
+	assert(servidor.estado == Estado.FIN_WAIT_2), f'El servidor no cambio a estado FIN_WAIT_2'
+
+def test8():
+	cliente = Nodo(Estado.ESTABLISHED)
+	servidor = Nodo(Estado.FIN_WAIT_1)
+	cliente.send(servidor, [Flag.ACK])
+	cliente.send(servidor, [Flag.FIN])
+	
+	# Se fija que al enviar el flag FIN el servidor queda en estado CLOSED
+	assert(servidor.estado == Estado.CLOSED), f'El servidor no cambio a estado CLOSED'
+
+def test():
+	### Invitacion a crear mas tests...
+	pass
+
+
+
+def correr_tests(*argv):
+	for i, test in enumerate(argv):
+		try:
+			test()
+			print(f'{OK}Test {i+1} esta bien!{ENDC}')
+		except Exception as e:
+			print(f'{FAIL}Error Test {i+1}: {str(e)}{ENDC}')
+
+if __name__ == '__main__':
+	correr_tests(
+				test1,
+				test2,
+				test3,
+				test4,
+				test5,
+				test6,
+				test7,
+				test8
+				)
---
+++ tcp.py
@@ -0,0 +1,69 @@
+from enum import Enum
+
+class Estado(Enum):
+	CLOSED = 1
+	LISTEN = 2
+	SYN_RCVD = 3
+	SYN_SENT = 4
+	ESTABLISHED = 5
+	FIN_WAIT_1 = 6
+	FIN_WAIT_2 = 7
+	CLISING = 8
+	TIME_WAIT = 9
+	CLOSE_WAIT = 10
+	LAST_ACK = 11
+
+	def __str__(self):
+		return f'{self.name}'
+
+class Flag(Enum):
+	ACK = 1
+	FIN = 2
+	SYN = 3
+
+	def __str__(self):
+		return f'{self.name}'
+
+class Nodo(object):
+	def __init__(self, estado_inicial=None):
+		if estado_inicial is None:
+			estado_inicial = Estado.CLOSED
+
+		# Variable que guarda el estado del nodo, inicialmente estan cerrados
+		self.stado = estado_inicial
+
+		# Variable que guarda a que nodo esta conectado este
+		self.conectado = None
+
+	def esta_conectado(self):
+		### Completar
+		pass
+
+	
+
+	# Esta funcion manda un paquete TCP, con flags, a otro Nodo
+	def send(self, destinatario, flags):
+		### Completar
+		pass
+
+	# Esta funcion se ejecuta cuando un nodo recibe un paquete TCP.
+	# Dependiendo que flags tenga el paquete, va a hacer una cosa u otra
+	def receive(self, emisor, flags):
+		### Completar
+		pass
+
+
+
+	# Hace que este nodo se conecte con el nodo 'destinatario'
+	# Esto significa pasar de estado CLOSED a ESTABLISHED,
+	# pasando por todos los pasos del protocolo TCP.
+	def handshake(self, destinatario):
+		### Completar
+		pass
+
+	# Hace que este nodo se desconecte del nodo conectado
+	# Esto significa pasar de estado ESTABLISHED a CLOSED,
+	# pasando por todos los pasos del protocolo TCP. 
+	def close(self, destinatario):
+		### Completar
+		pass
